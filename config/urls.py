f74e806bd2105ff6a48b74d9e1a7ecf94b55783a
Ilya Bratukhin <bratiliya@gmail.com>
2019-08-15 01:38:43 +0300

    added town stats view

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from yandex_bs.imports import views as import_views
 
 urlpatterns = [
-    path("imports", import_views.ImportCreateView.as_view(), name="create_import"),
-    path("imports/<int:import_id>/citizens", import_views.ImportRetrieveView.as_view(), name="retrieve_import"),
-    path("imports/<int:import_id>/citizens/birthdays", import_views.BirthdaysView.as_view(), name="birthdays"),
+    path("imports", import_views.create_import, name="create_import"),
+    path("imports/<int:import_id>/citizens", import_views.retrieve_import, name="retrieve_import"),
+    path("imports/<int:import_id>/citizens/birthdays", import_views.retrieve_birthdays, name="birthdays"),
+    path("imports/<int:import_id>/towns/stat/percentile/age", import_views.retrieve_town_stats, name="town_stats"),
 ]
--- yandex_bs/imports/views.py
+++ yandex_bs/imports/views.py
@@ -1,58 +1,75 @@
 from collections import defaultdict, Counter
 
+import numpy as np
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from yandex_bs.imports.models import ImportObject
 from yandex_bs.imports.seralizers import ImportSerializer, CitizenSerializer
+from yandex_bs.imports.utils import calculate_age
 
 
-class ImportCreateView(APIView):
-    def post(self, request):
-        import_serializer = ImportSerializer(data=request.data)
-        import_serializer.is_valid(raise_exception=True)
-        import_instance = import_serializer.save()
-        response_data = {
-            "data": {
-                "import_id": import_instance.id
-            }
+@api_view(['POST'])
+def create_import(equest):
+    import_serializer = ImportSerializer(data=request.data)
+    import_serializer.is_valid(raise_exception=True)
+    import_instance = import_serializer.save()
+    response_data = {
+        "data": {
+            "import_id": import_instance.id
         }
-        return Response(response_data, status=status.HTTP_201_CREATED)
+    }
+    return Response(response_data, status=status.HTTP_201_CREATED)
 
 
-class ImportRetrieveView(APIView):
-    def get(self, request, import_id):
-        import_object = get_object_or_404(ImportObject, pk=import_id)
-        citizens = import_object.citizens.order_by('citizen_id')
-        citizen_data = CitizenSerializer(citizens, many=True).data
-        return Response({"data": citizen_data})
+@api_view()
+def retrieve_import(request, import_id):
+    import_object = get_object_or_404(ImportObject, pk=import_id)
+    citizens = import_object.citizens.order_by('citizen_id')
+    citizen_data = CitizenSerializer(citizens, many=True).data
+    return Response({"data": citizen_data})
 
 
-class BirthdaysView(APIView):
-    def get(self, request, import_id):
-        import_object = get_object_or_404(ImportObject, pk=import_id)
+@api_view()
+def retrieve_birthdays(request, import_id):
+    import_object = get_object_or_404(ImportObject, pk=import_id)
 
-        birthdays_table = {}
+    birthdays_table = {}
 
-        for citizen_id, birth_date in import_object.citizens.filter(relatives__len__gt=0).values_list('citizen_id',
-                                                                                                      'birth_date'):
-            birthdays_table[citizen_id] = birth_date.month
+    for citizen_id, birth_date in import_object.citizens.filter(relatives__len__gt=0).values_list('citizen_id',
+                                                                                                  'birth_date'):
+        birthdays_table[citizen_id] = birth_date.month
 
-        calendar_table = defaultdict(Counter)
-        for citizen_id, relatives in import_object.citizens.filter(relatives__len__gt=0).values_list('citizen_id',
-                                                                                                     'relatives'):
-            for relative in relatives:
-                calendar_table[birthdays_table[relative]].update([citizen_id])
+    calendar_table = defaultdict(Counter)
+    for citizen_id, relatives in import_object.citizens.filter(relatives__len__gt=0).values_list('citizen_id',
+                                                                                                 'relatives'):
+        for relative in relatives:
+            calendar_table[birthdays_table[relative]].update([citizen_id])
 
-        response_data = {str(number): [] for number in range(1, 13)}
+    response_data = {str(number): [] for number in range(1, 13)}
 
-        for month_number in range(1, 13):
-            for citizen_id in calendar_table[month_number].keys():
-                response_data[str(month_number)].append({
-                    "citizen_id": citizen_id,
-                    "presents": calendar_table[month_number][citizen_id]
-                })
+    for month_number in range(1, 13):
+        for citizen_id in calendar_table[month_number].keys():
+            response_data[str(month_number)].append({
+                "citizen_id": citizen_id,
+                "presents": calendar_table[month_number][citizen_id]
+            })
 
-        return Response({"data": response_data})
+    return Response({"data": response_data})
+
+
+@api_view()
+def retrieve_town_stats(request, import_id):
+    import_object = get_object_or_404(ImportObject, pk=import_id)
+    town_table = defaultdict(list)
+    for town, birth_date in import_object.citizens.values_list('town', 'birth_date'):
+        town_table[town].append(calculate_age(birth_date))
+    response_data = []
+    for town in town_table.keys():
+        town_data = {"town": town}
+        for percentile_value in [50, 75, 99]:
+            town_data["p{}".format(str(percentile_value))] = np.percentile(town_table[town], percentile_value)
+        response_data.append(town_data)
+    return Response({"data": response_data})
